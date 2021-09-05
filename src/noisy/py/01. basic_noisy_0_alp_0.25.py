95a1188a8809399620d26c38d0ee3ac73bf2d605
kouriki <user@s197.lab>
2021-09-05 13:56:35 +0900

    make basic_noisy results

--- src/noisy/py/01. basic_noisy_0_alp_0.25.py
+++ src/noisy/py/01. basic_noisy_0_alp_0.25.py
@@ -31,11 +31,9 @@ pro = "asymmetric_squares"
 path = "../../../data/{}/".format(pro)
 
 layers = [3] + 8 * [20] + [2]
-# Itration = 1*10**5
-Itration = 1*10
+Itration = 1*10**5
 rs = 1234
-# N_train = .001
-N_train = .0001
+N_train = .001
 
 ns_lv = 0
 alpha = .25
